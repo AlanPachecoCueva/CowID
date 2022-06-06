9d1acee4d9363a0b0bd452930b35a2f315145e8f
alan.cueva@yahoo.es <alan.cueva@yahoo.es>
2022-06-06 16:52:13 -0500

    Avances en Login

--- App.js
+++ App.js
@@ -1,41 +1,58 @@
 //import { StatusBar } from 'expo-status-bar';
 import React from "react";
-import { StyleSheet, Text, SafeAreaView, View, StatusBar } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  View,
+  StatusBar,
+  Image,
+} from "react-native";
 
-{/*-----------------Importación de colores-----------------*/}
+{
+  /*-----------------Importación de colores-----------------*/
+}
 import colors from "./src/utils/colors";
 
-{/*-----------------Importación de componentes-----------------*/}
+{
+  /*-----------------Importación de componentes-----------------*/
+}
 import Form from "./src/components/Form";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 export default function App() {
   return (
     <>
-    {/* Barra de notificaciones del telefono (Parte Superior) */}
+      {/* Barra de notificaciones del telefono (Parte Superior) */}
       <StatusBar barStyle="light-content" />
+      <View style={styles.ViewContainer}>
+        <View style={styles.safeArea}>
+          {/*Cabecera */}
+          <Text style={styles.textoHead}>CowID</Text>
+          <Image
+            style={styles.img}
+            source={require("./src/utils/images/cowLogo.png")}
+          />
 
-      <View style={styles.safeArea}>
-        {/*Cabecera */}
-        <Text style={styles.textoHead}>CowID</Text>
+          <Form />
+        </View>
 
-        <Form/>
-      </View>
+        <View>{/* <Text>Resultado</Text> */}</View>
 
-      <View>
-        <Text>Resultado</Text>
-      </View>
-
-      <View>
-        <Text>Footer</Text>
+        <View>{/* <Text>Footer</Text> */}</View>
       </View>
     </>
   );
 }
 
 const styles = StyleSheet.create({
+  ViewContainer:{
+    height:"100%",
+    //backgroundColor:"#FAEEDC"
+  },
   safeArea: {
-    backgroundColor: colors.PRIMARY_COLOR_DARK,
-    height:"50%",
+    backgroundColor: colors.TERTIARY_COLOR,
+    height: "50%",
     borderBottomLeftRadius: 30,
     borderBottomRightRadius: 30,
     alignItems: "center",
@@ -44,6 +61,11 @@ const styles = StyleSheet.create({
     fontSize: 25,
     fontWeight: "bold",
     color: "#fff", //blanco
-    marginTop: 15,
+    marginTop: 30,
+  },
+  img: {
+    width: "50%",
+    height: "50%",
+    bottom: 0,
   },
 });
