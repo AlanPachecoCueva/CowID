54e003dd1a0b4cce9d758f368472e236917276f8
Alan Josueth Pacheco Cueva <alan.cueva@yahoo.es>
2022-06-20 00:06:34 -0500

    Generador de QR 2.0 Ahora genera con QR.toDataURL()

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,12 +1,67 @@
-import React from "react";
-import { View, Text } from "react-native";
+// import React from "react";
+// import { View, Text, Image } from "react-native";
 
+// // import QRCode from "react-native-qrcode-svg";
+// import QRCode from 'qrcode';
+
+
+// export default function HomeScreen() {
+
+
+// const base64img = QRCode.toDataURL('hello world');
+
+
+//   return (
+//     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+//       <Image value={base64img} />
+//     </View>
+//   );
+// }
+
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import { useRef } from "react";
 import QRCode from "react-native-qrcode-svg";
 
-export default function HomeScreen() {
+
+
+const QR = ({ name }) => {
+  let svg = useRef(null);
+
+  const getDataURL = () => {
+    svg?.toDataURL(callback);
+    //console.log(svg);
+  };
+
+  function callback(dataURL) {
+    console.log(dataURL);
+  }
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <QRCode value={"string"} />
+    <>
+      <QRCode size={300} value={`${name}`} getRef={(c) => (svg = c)} />
+
+      <Button onPress={getDataURL} title="Call Funct" color="#1FAAE2" />
+    </>
+  );
+};
+export default function HomeScreen() {
+  const input = useRef<TextInput>(null);
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+      <QR name="Holiiii" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
