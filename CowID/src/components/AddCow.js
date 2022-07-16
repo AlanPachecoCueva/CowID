334af8b44c7d1c5dd6f84460368e3c65e5ee75ce
Alan Josueth Pacheco Cueva <alan.cueva@yahoo.es>
2022-07-15 21:06:04 -0500

    Para unir

--- CowID/src/components/AddCow.js
+++ CowID/src/components/AddCow.js
@@ -1,18 +1,26 @@
-import React, { useState, useEffect } from "react"
-import { ScrollView, StyleSheet, 
-    Text, View, TextInput, Pressable, Switch } from "react-native";
+import React, { useState, useEffect, useRef } from "react"
+import { ScrollView, StyleSheet, PermissionsAndroid,Platform, Image,
+    Text, View, TextInput, Pressable, Switch, TouchableOpacity, Alert } from "react-native";
 
+    //Para guardar imagen
+    
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import colors from "../utils/colors";
 import QRCode from 'react-native-qrcode-svg';
 import DateTimePickerModal from "react-native-modal-datetime-picker";
+import {captureRef} from 'react-native-view-shot';
 
-
+import * as Permissions from 'expo-permissions';
+import * as MediaLibrary from 'expo-media-library'
+import CameraRoll from "@react-native-community/cameraroll"
 /* Metodos de la api*/
-import {getLastVaca, saveVaca} from '../apiRoutes/apiVaca'
+import {getLastVaca, saveVaca} from '../apiRoutes/apiVaca';
 
 export default function AddCow({ navigation, route }) {
+// create a ref
 
+
+    const componentRef = useRef();
     const [birthDateAux,setBirthDateAux] = useState('2022/07/16');
     const[cow,setCow] = useState({
         peso: 51.0, 
@@ -58,21 +66,84 @@ export default function AddCow({ navigation, route }) {
         setBirthDateAux(formatDate(date));
     };
 
-    
+    // get permission on android
+  const getPermissionAndroid = async () => {
+     try {
+       const granted = await PermissionsAndroid.request(
+         PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+         {
+           title: 'Image Download Permission',
+           message: 'Your permission is required to save images to your device',
+           buttonNegative: 'Cancel',
+           buttonPositive: 'OK',
+         },
+       );
+       if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+         return true;
+       }
+       Alert.alert(
+         '',
+         'Your permission is required to save images to your device',
+         [{text: 'OK', onPress: () => {}}],
+         {cancelable: false},
+       );
+     } catch (err) {
+       // handle error as you please
+       console.log('err', err);
+     }
+    //const perm = await Permissions.askAsync(Permissions.MEDIA_LIBRARY);
+
+  };
+
+  
 
     const [existCow, setExistCow] = useState(false);
     const [idCow, setIdCow] = useState();
+    const [qr, setQr] = useState(false);
+    
+    // download image
+  const downloadImage = async () => {
+    console.log("ENTRAAA");
+    try {
+      // react-native-view-shot caputures component
+      const uri = await captureRef(componentRef, {
+        format: 'png',
+        quality: 1,
+      });
 
+      if (Platform.OS === 'android') {
+        const granted = await getPermissionAndroid();
+        if (!granted) {
+          return;
+        }
+      }
+
+      // cameraroll saves image
+    //  const image = await CameraRoll.save
+    const image = MediaLibrary.saveToLibraryAsync(uri);
+      if (image) {
+        Alert.alert(
+          '',
+          'Image saved successfully.',
+          [{text: 'OK', onPress: () => {}}],
+          {cancelable: false},
+        );
+      }
+    } catch (error) {
+      console.log('error', error);
+    }
+  };
     const saveCow = async (vaca) => {
         //Guarda la vaca
-        //const res = await saveVaca(cow);
+        //console.log("Entra");
+        //const res = await saveVaca(vaca);
         
         //Recupera el id de la vaca generada
         const lastVaca = await getLastVaca();
         var s = lastVaca.split(",");
         var s2 = s[0].split(":");
         console.log("LastVaca: "+s2[1]);
-
+        setQr(true);
         setIdCow(s2[1]);
         //console.log("Cow: "+idCow);
         setExistCow(true);
@@ -93,79 +164,149 @@ export default function AddCow({ navigation, route }) {
         !isEnabled ? textChange("aptaParaProduccion",false) : textChange("aptaParaProduccion",true);
         console.log(cow.aptaParaProduccion);
     }
-
+    
     return (
-        <>
-            <ScrollView style={{ backgroundColor: "#ffffff" }}>
-                <View style={styles.formContainer}>
- 
-                    {/* Nacimiento*/}
-                    <Text style={styles.inputLabel}>Fecha de nacimiento</Text>
-                    <View style={[styles.inputHorizontalContainer,{marginTop:"1%"}]}>
-                        <TextInput placeholder="Seleccione una fecha" editable={true} keyboardType='number-pad'
-                        value={birthDateAux}
-                        style={styles.inputDate}/>
-                        <Pressable style={styles.buttonSalir} backgroundColor={colors.QUINARY_COLOR} onPress={showDatePicker}> 
-                            <Icon name="calendar" color={colors.SECONDARY_COLOR} size={25}/>
-                        </Pressable>
-                        <DateTimePickerModal
-                                isVisible={isDatePickerVisible}
-                                mode="date"
-                                onConfirm={handleConfirm}
-                                date={new Date(cow.fechaNacimiento)}
-                                onCancel={hideDatePicker}
-                        />
-                    </View>
+      <>
+        <ScrollView style={{ backgroundColor: "#ffffff" }}>
+          <View style={styles.formContainer}>
+            {/* Nacimiento*/}
+            <Text style={styles.inputLabel}>Fecha de nacimiento</Text>
+            <View
+              style={[styles.inputHorizontalContainer, { marginTop: "1%" }]}
+            >
+              <TextInput
+                placeholder="Seleccione una fecha"
+                editable={true}
+                keyboardType="number-pad"
+                value={birthDateAux}
+                style={styles.inputDate}
+              />
+              <Pressable
+                style={styles.buttonSalir}
+                backgroundColor={colors.QUINARY_COLOR}
+                onPress={showDatePicker}
+              >
+                <Icon
+                  name="calendar"
+                  color={colors.SECONDARY_COLOR}
+                  size={25}
+                />
+              </Pressable>
+              <DateTimePickerModal
+                isVisible={isDatePickerVisible}
+                mode="date"
+                onConfirm={handleConfirm}
+                date={new Date(cow.fechaNacimiento)}
+                onCancel={hideDatePicker}
+              />
+            </View>
 
-                    {/*  Peso */}
-                    <Text style={styles.inputLabel}>Peso</Text>
-                    <TextInput placeholder="Peso"  keyboardType="number-pad" style={styles.inputText} 
-                    onChangeText={(val) => textChange("peso",val)} />
-                    {/* partos */}
-            
-                    <Text style={styles.inputLabel}>Cantidad de partos</Text>
-                    <TextInput placeholder="Cantidad de partos" keyboardType="decimal-pad" style={styles.inputText}
-                        onChangeText={(val) => textChange("numeroPartos",val)} />
+            {/*  Peso */}
+            <Text style={styles.inputLabel}>Peso</Text>
+            <TextInput
+              placeholder="Peso"
+              keyboardType="number-pad"
+              style={styles.inputText}
+              onChangeText={(val) => textChange("peso", val)}
+            />
+            {/* partos */}
 
-                    {/* ubicación */}
-                    
-                    <Text style={styles.inputLabel} >Ubicación</Text>
-                    <TextInput placeholder="Parcelas" keyboardType="ascii-capable" style={styles.inputText} onChangeText={(val) => textChange("parcelaUbicacion",val)} />
-                    
-                    <Text style={styles.inputLabel}>Edad de destete</Text>
-                    <TextInput placeholder="Edad destete" keyboardType="number-pad" style={styles.inputText}
-                        onChangeText={(val) => textChange("edadDestete",val)} />
+            <Text style={styles.inputLabel}>Cantidad de partos</Text>
+            <TextInput
+              placeholder="Cantidad de partos"
+              keyboardType="decimal-pad"
+              style={styles.inputText}
+              onChangeText={(val) => textChange("numeroPartos", val)}
+            />
 
-                    {/* produccion */}
-                    <View style={styles.inputHorizontalContainer}>
-                        <Text style={{paddingRight:20,fontSize:20}}>¿Está produciendo?</Text>
-                        <Switch trackColor={{ false: "#767577", true: "#c7934e" }}
-                            thumbColor={isEnabled ? "#271d14" : "#271d14"}
-                            onValueChange={toggleSwitch}
-                            value={isEnabled}></Switch>
-                    </View>
+            {/* ubicación */}
 
-                    {/* Boton para agregar la vaca */}
-                    <Pressable style={styles.buttonContainer} backgroundColor={colors.PRIMARY_COLOR} onPress={() => {saveCow(cow)}}> 
-                            <Icon name="plus-box" style={{paddingRight:10}} color={colors.SECONDARY_COLOR} size={25}/>
-                            <Text style={{ fontSize: 18 ,color:colors.SECONDARY_COLOR,fontWeight:"bold"}}>Agregar</Text>
-                    </Pressable>
+            <Text style={styles.inputLabel}>Ubicación</Text>
+            <TextInput
+              placeholder="Parcelas"
+              keyboardType="ascii-capable"
+              style={styles.inputText}
+              onChangeText={(val) => textChange("parcelaUbicacion", val)}
+            />
 
-                    
-                    {/* QR CODE */}
-                    {existCow ? <QRCode value={idCow}codeStyle="square"/>: <></>}
-                    
-                    
-                </View>
-            </ScrollView>
-        </>
-    )
+            <Text style={styles.inputLabel}>Edad de destete</Text>
+            <TextInput
+              placeholder="Edad destete"
+              keyboardType="number-pad"
+              style={styles.inputText}
+              onChangeText={(val) => textChange("edadDestete", val)}
+            />
+
+            {/* produccion */}
+            <View style={styles.inputHorizontalContainer}>
+              <Text style={{ paddingRight: 20, fontSize: 20 }}>
+                ¿Está produciendo?
+              </Text>
+              <Switch
+                trackColor={{ false: "#767577", true: "#c7934e" }}
+                thumbColor={isEnabled ? "#271d14" : "#271d14"}
+                onValueChange={toggleSwitch}
+                value={isEnabled}
+              ></Switch>
+            </View>
+
+            {/* Boton para agregar la vaca */}
+            <Pressable
+              style={styles.buttonContainer}
+              backgroundColor={colors.PRIMARY_COLOR}
+              onPress={() => {
+                saveCow(cow);
+              }}
+            >
+              <Icon
+                name="plus-box"
+                style={{ paddingRight: 10 }}
+                color={colors.SECONDARY_COLOR}
+                size={25}
+              />
+              <Text
+                style={{
+                  fontSize: 18,
+                  color: colors.SECONDARY_COLOR,
+                  fontWeight: "bold",
+                }}
+              >
+                Agregar
+              </Text>
+            </Pressable>
+
+            {/* QR CODE */}
+            {/* {existCow ? 
+              <> */}
+              <View collapsable={false} style={styles.qrStyle} ref={componentRef} >
+              <QRCode size="1500" value={idCow} style={styles.qrStqryle} codeStyle="square" /> 
+                
+                </View>                  
+                
+                <TouchableOpacity onPress={downloadImage}>
+                  <Text>Save</Text>
+                </TouchableOpacity>
+              {/* </>
+             : 
+              <></>
+            } */}
+          </View>
+        </ScrollView>
+      </>
+    );
 }
 
 /**Los colores son : cafe oscuro: #271d14 para el fondo y blanco para los cuadros de texto */
 /**pendiente mejorar el manejo de height y width */
 const styles = StyleSheet.create({
-
+    qrStyle:{
+        backgroundColor:"red",
+        height:300,
+        alignContent:"center"
+    },
+    qr:{
+        
+    },
     formContainer: {
         paddingHorizontal:25,
         paddingBottom:25,
