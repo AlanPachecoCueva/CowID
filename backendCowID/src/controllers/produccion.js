6d5da8876c3c0c44257b0e5a9b15c4cb5aa6215f
ErikMurmi <erikmurmi@gmail.com>
2023-02-18 14:59:09 -0500

    se puede crear una produccion diaria

--- backendCowID/src/controllers/produccion.js
+++ backendCowID/src/controllers/produccion.js
@@ -1,13 +1,25 @@
 import { connect } from '../database';
+import { Collections } from './collections';
+import { doc, updateDoc, arrayUnion, arrayRemove } from 'firebase/firestore';
+import { db } from '../config/firebase';
 
 export const getProducciones = async (req, res) => {
     const db = await connect();
-    
+
     const [rows] = await db.query("SELECT * FROM ProduccionDiaria");
 
     console.log([rows]);
     res.json([rows]);
 }
+
+// export const getProduccion = async (req, res) => {
+//     const db = await connect();
+
+//     const [rows] = await db.query("SELECT * FROM ProduccionDiaria WHERE id = ?", [req.params.id]);
+
+//     console.log(rows[0]);
+//     res.json(rows[0]);
+// }
 
 export const getProduccion = async (req, res) => {
     const db = await connect();
@@ -34,23 +46,39 @@ export const getProduccionesCount = async (req, res) => {
     res.json(rows[0]["COUNT(*)"]);
 }
 
+// export const createProduccion = async (req, res) => {
+//     const db = await connect();
+//     const [results] = await db.query("INSERT INTO ProduccionDiaria(VacaID, Fecha, CantidadManana, CantidadTarde)"+
+//     "VALUES (? ,?, ?, ?) ",
+//          [req.body.VacaID, req.body.Fecha, req.body.CantidadManana, req.body.CantidadTarde]);
+
+//     console.log({
+//         id : results.insertId,
+//         ...req.body
+//         });
+
+//         /*El "..." selecciona todo lo del req.body */
+
+//     res.json({
+//         id : results.insertId,
+//         ...req.body
+//         });
+// }
 export const createProduccion = async (req, res) => {
-    const db = await connect();
-    const [results] = await db.query("INSERT INTO ProduccionDiaria(VacaID, Fecha, CantidadManana, CantidadTarde)"+
-    "VALUES (? ,?, ?, ?) ",
-         [req.body.VacaID, req.body.Fecha, req.body.CantidadManana, req.body.CantidadTarde]);
+    try {
+        const { body } = req
+        const vacaRef = doc(db, Collections.VACAS, body.VacaID);
+        const { Fecha, CantidadManana, CantidadTarde } = body
 
-    console.log({
-        id : results.insertId,
-        ...req.body
+        await updateDoc(vacaRef, {
+            produccionesDiarias: arrayUnion({ "fecha": Fecha, "mañana": CantidadManana, "tarde": CantidadTarde })
         });
 
-        /*El "..." selecciona todo lo del req.body */
-
-    res.json({
-        id : results.insertId,
-        ...req.body
-        });
+        return res.status(200).json({ ok: true })
+    }catch(e){
+        
+    }
+    
 }
 
 export const deleteProduccion = async (req, res) => {
@@ -65,7 +93,7 @@ export const deleteProduccion = async (req, res) => {
 export const updateProduccion = async (req, res) => {
     const db = await connect();
 
-    const results = await db.query('UPDATE ProduccionDiaria SET ? WHERE id = ?',[
+    const results = await db.query('UPDATE ProduccionDiaria SET ? WHERE id = ?', [
         req.body,
         req.params.id
     ])
